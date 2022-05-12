1729984619ea23d7b8de414cd503c88f38f91a9e
Re3ee0 <93414995+Re3ee0@users.noreply.github.com>
2022-05-12 15:08:54 +0800

    Delete SiteAalysis directory

--- SiteAalysis/googleapi.py
+++
@@ -1,2 +0,0 @@
-https://maps.googleapis.com/maps/api/place/textsearch/json?query=places+in+'London'&key='AIzaSyD_bI_39EXhJmjCugypjLu86OwknIGFPuA'
-https://maps.googleapis.com/maps/api/place/textsearch/json?query=todo+in+India&key='AIzaSyD_bI_39EXhJmjCugypjLu86OwknIGFPuA'
